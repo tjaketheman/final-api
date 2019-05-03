3cf29abfb60ca5f7824264c6a5052122db1d65cf
Trevor <tjaketheman@yahoo.com>
2019-05-02 22:36:51 -0600

    put returns something now, just not the right thing.

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const port = process.env.PORT || 5005;
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').ObjectID;
 const ConnectionURL = "mongodb+srv://tjaketheman:owner1@mycluster-zfcy6.mongodb.net/test?retryWrites=true";
 const dbName = "Pantrack";
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const pantrySchema = new Schema({
-    name: {type: String, required: true, max: 20},
-    amount: {type: Number, required: true},
-    unit: {type: String, required: true}
+    name: { type: String, required: true, max: 20 },
+    amount: { type: Number, required: true },
+    unit: { type: String, required: true }
 
-});
+}, {collection: 'Users'});
 
-module.exports = mongoose.model('Pantry', pantrySchema);
+const pantryModel = mongoose.model('pantryModel', pantrySchema);
 
 app.listen(port, () => {
     MongoClient.connect(ConnectionURL, { useNewUrlParser: true }, (error, client) => {
@@ -33,16 +34,16 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true }));
 
-app.param('id', (req, res, next, id) =>{
-    const item = _.find(items, {_id: _id});
+// app.param('id', (req, res, next, id) =>{
+//     const item = _.find(items, {_id: _id});
 
-    if (item) {
-        req.item = item;
-        next();
-    } else {
-        res.send(err);
-    }
-})
+//     if (item) {
+//         req.item = item;
+//         next();
+//     } else {
+//         res.send(err);
+//     }
+// })
 
 app.get('/pantry', (req, res) => {
     collection.find({}).toArray((err, result) => {
@@ -60,8 +61,6 @@ app.get('/pantry/:id', (req, res) => {
             }
         res.send(result);
     })
-
-
 })
 
 app.post('/pantry/add', cors(), (req, res) => {
@@ -75,11 +74,21 @@ app.post('/pantry/add', cors(), (req, res) => {
 })
 
 app.put('/pantry/:id', (req, res) => {
-    collection.findOne(req.params._id, (err, result) => {
-        if(err) {
-            return res.status(500).send(err)
-        }
-    }))
+    const data = {
+        name: req.body.name,
+        amount: req.body.amount,
+        unit: req.body.unit,
+        id: req.body._id
+    };
+    console.log(data)
+    // const id = req.body.id;
+    collection.updateOne({"_id": req.body._id}, {$set: data}, (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+            }
+        res.send(result);
+    })
+})
 
 app.delete('/pantry/:id', (req, res) => {
     collection.findOne(req.params._id, )
